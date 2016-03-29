86db31b954019308728df55ea2eb22872996a856
EthanR <rivettea@hotmail.com>
2016-03-29 04:41:00 -0400

    Initial Commit for SRM 686 Problem 1
    The initial commit for SRM 686 Problem 1 Solution. For more information
    see the Problem Statement.

---
+++ SRM686/SRM686/TreeAndVertex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM686
+{
+    public class TreeAndVertex
+    {
+        static void Main(string[] args)
+        {
+            int[] tree = {0, 0, 0};
+            Console.WriteLine(get(tree));
+        }
+
+        public static int get(int[] tree)
+        {
+            int firstVertex = tree[0];
+            int result;
+
+            IEnumerable<IGrouping<int,int>> groups = tree.GroupBy(x => x);
+            IGrouping<int, int> largest = groups.OrderByDescending(x => x.Count()).First();
+
+            if (tree.All(vertex => vertex == firstVertex))
+            {
+                result = largest.Count();
+            }
+            else
+            {
+                result = largest.Count() + 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SRM686/TreeAndVertexTests/TreeAndVertexTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using SRM686;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SRM686Tests
+{
+    [TestClass]
+    public class TreeAndVertexTests
+    {
+        [TestMethod]
+        public void GetTestCaseOne()
+        {
+            // Arrange
+            int[] tree = {0, 0, 0};
+            int expected = 3;
+
+            // Act
+            int result = TreeAndVertex.get(tree);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GetTestCaseTwo()
+        {
+            // Arrange
+            int[] tree = {0, 1, 2, 3};
+            int expected = 2;
+
+            // Act
+            int result = TreeAndVertex.get(tree);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GetTestCaseThree()
+        {
+            // Arrange
+            int[] tree = {0, 0, 2, 2};
+            int expected = 3;
+
+            // Act
+            int result = TreeAndVertex.get(tree);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GetTestCaseFour()
+        {
+            // Arrange
+            int[] tree = {0, 0, 0, 1, 1, 1};
+            int expected = 4;
+
+            // Act
+            int result = TreeAndVertex.get(tree);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GetTestCaseFive()
+        {
+            // Arrange
+            int[] tree = {0, 1, 2, 0, 1, 5, 6, 1, 7, 4, 2, 5, 5, 8, 6, 2, 14, 12, 18, 10, 0, 6, 18, 2, 20, 11, 0, 11, 7, 12, 17, 3, 18, 31, 14, 34, 30, 11, 9};
+            int expected = 5;
+
+            // Act
+            int result = TreeAndVertex.get(tree);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
