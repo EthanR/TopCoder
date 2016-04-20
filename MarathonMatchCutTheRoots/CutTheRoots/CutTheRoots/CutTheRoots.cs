a41d17a3d54d547c65861d28d69cd0e7735736ee
EthanR <rivettea@hotmail.com>
2016-04-20 01:38:42 -0400

    Changed test cases for CutTheRoots
    Made a realistic and simplistic test case for CutTheRoots.

--- MarathonMatchCutTheRoots/CutTheRoots/CutTheRoots/CutTheRoots.cs
+++ MarathonMatchCutTheRoots/CutTheRoots/CutTheRoots/CutTheRoots.cs
@@ -8,7 +8,31 @@ namespace CutTheRoots
     {
         public static void Main(string[] args)
         {
-            int[] cuts = makeCuts(5, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new int[] { 0, 1, 2, 3, 4 });
+            int[] testPoints =
+            {
+                 0, 0,   11, 11,   22, 22,   33, 33,   44, 44,                      
+                 1, 1,   12, 12,   23, 23,   34, 34,   45, 45,
+                 2, 2,   13, 13,   24, 24,   35, 35,   46, 46,
+                 3, 3,   14, 14,   25, 25,   36, 36,   47, 47,
+                 4, 4,   15, 15,   26, 26,   37, 37,   48, 48,
+                 5, 5,   16, 16,   27, 27,   38, 38,   49, 49,
+                 6, 6,   17, 17,   28, 28,   39, 39,   50, 50,
+                 7, 7,   18, 18,   29, 29,   40, 40,   51, 51,
+                 8, 8,   19, 19,   30, 30,   41, 41,   52, 52,
+                 9, 9,   20, 20,   31, 31,   42, 42,   53, 53,
+               10, 10,   21, 21,   32, 32,   43, 43,   54, 54
+            };
+
+            int [] testRoots =
+            {
+               0, 5,   5, 10,   10, 15,   15, 20,   20, 25,   25, 30,   30, 35,   35, 40,   40, 45,   45, 50,
+               1, 6,   6, 11,   11, 16,   16, 21,   21, 26,   26, 31,   31, 36,   36, 41,   41, 46,   46, 51,
+               2, 7,   7, 12,   12, 17,   17, 22,   22, 27,   27, 32,   32, 37,   37, 42,   42, 47,   47, 52,
+               3, 8,   8, 13,   13, 18,   18, 23,   23, 28,   28, 33,   33, 38,   38, 43,   43, 48,   48, 53,
+               4, 9,   9, 14,   14, 19,   19, 24,   24, 29,   29, 34,   34, 39,   39, 44,   44, 49,   49, 54 
+            };
+
+            int[] cuts = makeCuts(5, testPoints, testRoots);
             foreach (int cut in cuts)
             {
                 Console.WriteLine(cut);
@@ -59,12 +83,12 @@ namespace CutTheRoots
                     {
                         StartPoint = new Point
                             {
-                                XCoordinate = orderedPoints[i].XCoordinate,
+                                XCoordinate = orderedPoints[i].XCoordinate + 1,
                                 YCoordinate = orderedPoints[i].YCoordinate + 1
                             },
                         EndPoint = new Point
                             {
-                                XCoordinate = orderedPoints[i].XCoordinate,
+                                XCoordinate = orderedPoints[i].XCoordinate + 1,
                                 YCoordinate = orderedPoints[i].YCoordinate - 1
                             }
                     });
