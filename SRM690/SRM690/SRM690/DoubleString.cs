a1e3e12cdd9c169cbd079ea726133a9725a89491
EthanR <rivettea@hotmail.com>
2016-05-05 07:51:16 -0400

    Commit for SRM 690 DoubleString
    Commit for SRM 690 DoubleString

---
+++ SRM690/SRM690/SRM690/DoubleString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM690
+{
+    class DoubleString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Check("HAHA"));
+        }
+
+        public static string Check(string potentialString)
+        {
+            if (potentialString.Length == 1)
+            {
+                return "not square";
+            }
+
+            string firstHalf = potentialString.Substring(0, (potentialString.Length / 2));
+            string secondHalf = potentialString.Substring((potentialString.Length / 2));
+            return firstHalf == secondHalf ? "square" : "not square";
+        }
+    }
+}
